097f16afa029d132b6037f6ab3fab47f518044df
musavir <mir.musavir14@gmail.com>
2026-08-18 17:56:20 +0530

    Add: string-compression - submission-0

---
+++ Data Structures & Algorithms/string-compression/submission-0.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int compress(vector<char>& chars) {
+        int read = 0;
+        int write = 0;
+        int  n = chars.size();
+        while(read < n){
+            char ch = chars[read]; // a
+            int count = 0; 
+             
+            while(read<n && chars[read] == ch ){
+                count++; 
+                read++;
+            }
+
+            if(count == 1){
+                chars[write++] = ch;
+            }
+            else{
+                chars[write++] = ch;
+                string digit = to_string(count);
+                for(char c: digit){
+                    chars[write++] = c;
+                }
+            }
+        }
+        
+        return write;
+
+    }
+};
